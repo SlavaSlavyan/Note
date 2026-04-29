0c5abb86fa3c99ed616c0ad0b0f4090ec4c2f9de
Svyatoslav <tortik2000.g@gmail.com>
2026-04-29 12:13:52 +0300

    Тестик vscode

---
+++ 290426/test/src/TestClass.cpp
@@ -0,0 +1,5 @@
+#include "../include/TestClass.hpp"
+
+double TestClass::getAVG() {
+    return 0.0;
+}
---
+++ 290426/test/test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "include\TestClass.hpp"
+
+int main() 
+{
+    TestClass test;
+
+    std::cout << test.getAVG();
+
+    return 0;
+}
